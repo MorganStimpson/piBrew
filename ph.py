9f0c143a33a582fc66654997460ea02f73fe10fe
MorganStimpson <morgan.stimpson@hotmail.com>
2021-08-30 15:35:22 -0700

    Made a new branch

---
+++ ph.py
@@ -0,0 +1,7 @@
+# Morgan Stimpson (morgan.stimpson@hotmail.com)
+# PH SENSING PROGRAM
+# PH MONITOR https://www.amazon.com/Atlas-Scientific-pH-Kit-0-14/dp/B00798612C/ref=pd_bxgy_img_1/147-1633447-5966414?pd_rd_w=gUgvp&pf_rd_p=c64372fa-c41c-422e-990d-9e034f73989b&pf_rd_r=32HVVYJBWGF79ETZZFAY&pd_rd_r=ccf8fb9e-5f57-4920-b9a9-08f311b734af&pd_rd_wg=eN6ys&pd_rd_i=B00798612C&psc=1
+# SENSOR: https://www.amazon.com/Atlas-Scientific-Consumer-Grade-Probe/dp/B07VDMNB92/ref=pd_sbs_4/147-1633447-5966414?pd_rd_w=uE906&pf_rd_p=cc1c437d-ebe7-44ed-9018-890d5eab4e8d&pf_rd_r=XWNBXREYWV72JH1JZ0RH&pd_rd_r=bb5e8b58-7807-4c90-9109-0309c8986eb6&pd_rd_wg=8If66&pd_rd_i=B07VDMNB92&psc=1
+# ADAPTER: https://www.amazon.com/Atlas-Scientific-Consumer-Grade-Probe/dp/B07VDMNB92/ref=pd_sbs_4/147-1633447-5966414?pd_rd_w=uE906&pf_rd_p=cc1c437d-ebe7-44ed-9018-890d5eab4e8d&pf_rd_r=XWNBXREYWV72JH1JZ0RH&pd_rd_r=bb5e8b58-7807-4c90-9109-0309c8986eb6&pd_rd_wg=8If66&pd_rd_i=B07VDMNB92&psc=1
+
+# Trying to cut costs of the initial kit
