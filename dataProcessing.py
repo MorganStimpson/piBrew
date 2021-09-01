41dde27c792c56fa5492c5689f5aec2bf59f596f
Morgan <morgan.stimpson@hotmail.com>
2021-09-01 15:28:48 -0700

    Update dataProcessing.py

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,7 +1,31 @@
 # Morgan Stimpson (morgan.stimpson@Hotmail.com)
+# piBrew - data processing
+# The intention of this pieces is to process the data that occurs from running the raspbery pi
+#  and it's extras
 
+# I do not know of other graphs that would help
+
+# to call other functions from a different file 
+import piBrew
+import matplotlib
+
+# Scatter Plot
+
+# Boxplot
+
+# Line Graph
+def lineGraph():
+    print("Starting the line graph")
+
+
+# Main
 def main():
     print("Starting data processing")
+    print("What type of graph would you like to see?")
+    print("Which elements would you like to observe? Time, Temp, O2, CO2, PH")
+
+    # connect to db
+    # disconnect from db
 
 if __name__ == '__main__':
     main()
