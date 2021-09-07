cb8fdba5f3b1052828b24a7ddc3aa57e675a4b71
Morgan <morgan.stimpson@hotmail.com>
2021-09-07 12:20:55 -0700

    Temperature reading is going correctly.
    Inserting temp to master. I will keep temp around just to test here and there.

--- piBrew.py
+++ piBrew.py
@@ -18,12 +18,24 @@
 # Want to update from sqlite to mySQL, because I am more fimiliar
 
 # IMPORT SECTION
+from glob import glob
 import sqlite3
+import os
+import glob
+import time
 #import pandas as pd
 
 from datetime import date, datetime
 from time import sleep
 # import RPi.GPIO as GPIO
+
+# GLOBALS
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
 
 # ======================= SENSORS ========================================
 
@@ -40,12 +52,29 @@ def ReadFromSensors():
 
     return temperature, o2, co2, ph
 
+# ==== Tempearture Reading ===
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def pullTempReading():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = ((temp_c * 9) / 5) + 32
+        return temp_f
+# ===============
+
 # This is the sensor section.
 # I can not quiet read from these yet as I do not have a raspberry Pi to work with
 # https://medium.com/initial-state/how-to-build-a-raspberry-pi-temperature-monitor-8c2f70acaea9
-def pullTempReading():
-    temp = 0
-    return temp
 
 def pullO2Reading():
     o2 = 1
