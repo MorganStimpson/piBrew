16cc33ac9593203fa1899f28498562c4be9d6b2a
Morgan <morgan.stimpson@hotmail.com>
2021-09-08 12:50:35 -0700

    Simple Data Visualization Update
    I thought it would be nice to see what the data looks like as it processes.

--- dataProcessing.py
+++ dataProcessing.py
@@ -6,16 +6,33 @@
 # I do not know of other graphs that would help
 
 # to call other functions from a different file 
-import piBrew
-import matplotlib
+#import piBrew
+import matplotlib.pyplot as plt
+import sqlite3
+import pandas as pd
+import numpy as np
+from pprint import pprint
 
 # Scatter Plot
 
 # Boxplot
 
 # Line Graph
+# work on this
 def lineGraph():
     print("Starting the line graph")
+
+    conn = sqlite3.connect('FERMENTATION.db')
+    sql = "SELECT TEMPERATURE FROM FERMENTATION "
+
+    df = pd.read_sql(sql, conn)
+    
+    print(df.head())
+    print(df.tail())
+
+    plt.plot(df)
+    plt.show()
+    
 
 
 # Main
@@ -24,8 +41,17 @@ def main():
     print("What type of graph would you like to see?")
     print("Which elements would you like to observe? Time, Temp, O2, CO2, PH")
 
+    lineGraph()
+
     # connect to db
+    connection = sqlite3.connect('FERMENTATION.db') # this will make a db if none are found -- if there is one skip
+    cursor = connection.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
     # disconnect from db
+    connection.close() # don't forget to close the db when you're finsihed
+
+
 
 if __name__ == '__main__':
     main()
